8c890e6bdd510e64fba688e7be983c03df9ac559
Logan Negley <logannegley@gmail.com>
2020-04-26 18:27:32 -0500

    migration table created

--- migrations/20200426181412_cars-schema.js
+++ migrations/20200426181412_cars-schema.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('cars', table =>{
+      table.increments();
+      table.string('VIN').unique().notNullable();
+      table.string('make').notNullable();
+      table.string('model').notNullable();
+      table.integer('mileage').notNullable();
+      table.string('transmission');
+      table.string('title');
+  });
 };
 
 exports.down = function(knex) {
-  
+knex.schema.dropTableIfExists('cars')
 };
