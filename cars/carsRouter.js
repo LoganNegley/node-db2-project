0940937642266d3c927c3b26e6aae018f4e0440d
Logan Negley <logannegley@gmail.com>
2020-04-26 17:57:33 -0500

    getting all cars from database endpoint working

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -5,6 +5,16 @@ const db = require('../data/db.config');
 
 const router = express.Router();
 
-
+router.get('/', (req,res) =>{
+db.select().from('cars')
+.then(cars =>{
+    res.status(200).json(cars)
+})
+.catch(error =>{
+    res.status(500).json({
+        errorMessage: 'Unable to get car information'
+    })
+})
+});
 
 module.exports = router;
