79f952315de855d756b21d9cad7f4eee0ab784cd
Logan Negley <logannegley@gmail.com>
2020-04-26 17:38:56 -0500

    no changes

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const server = require('./api/server');
 
-const PORT = process.env.PORT || 5000;
-
-server.listen(PORT, () =>{
-    console.log(`\n== API running on port ${PORT} ==\n`);
+server.listen(5000, () =>{
+    console.log(`API running on port 5000` );
 });
