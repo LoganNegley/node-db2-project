94a9dff0bdaeeb303c1761eb2189bcffdc89e608
Logan Negley <logannegley@gmail.com>
2020-04-26 18:41:18 -0500

    migrations file in the wrong folder. Migration created

--- knexfile.js
+++ knexfile.js
@@ -7,7 +7,7 @@ module.exports = {
     connection: {
       filename: './data/car-dealer.db3'
     }, 
-    useNullAsDefault:true
+    useNullAsDefault:true,
     migrations: {
       directory: './data/migrations'
     }
