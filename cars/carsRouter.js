1c23e877010dd3c4d14adc8fcfb4a24047b1d84c
Logan Negley <logannegley@gmail.com>
2020-04-28 16:07:15 -0500

    delete car by ID done

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -17,4 +17,65 @@ db.select().from('cars')
 })
 });
 
+router.get('/:id', (req,res) =>{
+    db.select().from('cars').where('id', req.params.id)
+    .then(car =>{
+        if(car && car.length !== 0){
+            res.status(200).json(car)
+        }else{
+            res.status(404).json({
+                message:'Car with that ID does not exist'
+            })
+        }
+    })
+    .catch(error =>{
+        res.status(500).json({
+            errorMessage:'Unable to get data'
+        })
+    })
+})
+
+router.post('/', (req,res) =>{
+    const payLoad = {
+            VIN: req.body.VIN, 
+            make:req.body.make, 
+            model:carData.model, 
+            mileage:carData.mileage, 
+            transmission:carData.transmisson, 
+            title:carData.title }
+    db('cars')
+    .insert(payLoad)
+    .then(car =>{
+        res.status(201).json(car)
+    })
+    .catch(error =>{
+        res.status(500).json({
+            errorMessage:'Car info unable to be added'
+        })
+    })
+})
+
+router.delete('/:id', (req, res) =>{
+    db('cars')
+    .where('id', req.params.id)
+    .del()
+    .then(car =>{
+        if(car && car.length !== 0){  
+            res.status(200).json(car)
+        } else{
+            res.status(404).json({
+                message: 'Car with that ID does not exist'
+            })
+        }
+    })
+    .catch(error =>{
+        res.status(500).json({
+            errorMessage: 'Unable to delete car'
+        })
+    })
+})
+
+
+
+
 module.exports = router;
